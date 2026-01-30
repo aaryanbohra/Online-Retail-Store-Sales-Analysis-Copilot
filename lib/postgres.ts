e3e14f71e057de6dc9bc32b27cc3b6bb309f7680
Aaryan Bohra <aaryanbohra@yahoo.com>
2026-01-29 22:27:03 -0800

    Remove invalid postgres config options

--- lib/postgres.ts
+++ lib/postgres.ts
@@ -6,9 +6,4 @@ if (!connectionString) {
   throw new Error('DATABASE_URL or POSTGRES_URL environment variable is required');
 }
 
-export const sql = postgres(connectionString, {
-  max: 10,
-  idle_timeout: 30,
-  connect_timeout: 10,
-  statement_timeout: 60000, // 60 seconds for complex queries
-});
+export const sql = postgres(connectionString);
